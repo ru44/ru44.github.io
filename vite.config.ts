388421ff98465c31eab498051f90dad25a514de5
RuM <18311071+ru44@users.noreply.github.com>
2024-06-22 15:28:47 +0300

    update some file here and there

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueDevTools from 'vite-plugin-vue-devtools'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 // https://vitejs.dev/config/
@@ -10,6 +11,7 @@ export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    vueDevTools(),
     Components({
       dts: true
     }),
